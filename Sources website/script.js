5a10d7ef3af1da930237d1b6d61edebc39e7d5f3
JAM0005 <81664545+JAM0005@users.noreply.github.com>
2022-03-03 09:56:25 +1100

    Update script.js

--- Sources website/script.js
+++ Sources website/script.js
@@ -12,14 +12,19 @@ textToEnter = `
 Gray, N. Simkiss, N. Snowden, R. Walters, T. (2021). Secondary school students’ perception of the online 
 teaching experience during COVID-19: The impact on mental wellbeing and specific learning difficulties. 
 https://bpspsychub.onlinelibrary.wiley.com/doi/10.1111/bjep.12475
+
 Belli, B. (2020). National survey: Students’ feelings about high school are mostly negative. 
 https://news.yale.edu/2020/01/30/national-survey-students-feelings-about-high-school-are-mostly-negative 
+
 Fleming, S. (2021). Kids learn better in class than when studying from home, finds teacher survey. 
 https://www.weforum.org/agenda/2021/03/classroom-teaching-better-than-remote-learning-education/
+
 Lecture Talks. (2021, June 21) Online Learning Pros and Cons Vs.Onsite Learning. 
 https://lecturetalks.com/online-learning-pros-and-cons-vs-onsite-learning/
+
 High Focus Centers. (2020, November 20). The Effects of Online Learning on a Teen’s Mental Health. 
 https://highfocuscenters.pyramidhealthcarepa.com/the-effects-of-online-learning-on-a-teens-mental-health/
+
 ATAR Notes. (2020, April 25). TOPIC: IS REMOTE LEARNING SOMEHOW AN ADVANTAGE FOR YEAR 12'S?. 
 https://atarnotes.com/forum/index.php?topic=190922.0
 
